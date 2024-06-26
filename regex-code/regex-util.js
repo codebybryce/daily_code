d08a8882488af3499b04635b860509e67ca97d9c
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-06-26 12:12:22 -0400

    Daily Update - Find incorrect HTML nesting

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -1,1 +1,2 @@
 const regexArray = [{ undefined: { description: "Matches the undefined value", regex: /^undefined$/ } }];
+const patterns = []; patterns.push({ hexEscape: { description: "Extract hexadecimal escape sequences", regex: /\\x[0-9A-Fa-f]{2}/g } });
