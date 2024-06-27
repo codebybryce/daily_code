96b37fc8e03c8f1b4f47ff749f2d004ccd846d4b
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-06-27 12:12:52 -0400

    Daily Update - Match screenplay formatting rules

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -1,2 +1,3 @@
 const regexArray = [{ undefined: { description: "Matches the undefined value", regex: /^undefined$/ } }];
 const patterns = []; patterns.push({ hexEscape: { description: "Extract hexadecimal escape sequences", regex: /\\x[0-9A-Fa-f]{2}/g } });
+const regexPatterns = [{ key: 'incorrectNesting', description: 'Finds incorrect HTML nesting', regex: /<([a-z]+)>(?:(?!<\/\1>).)*<([a-z]+)>(?:(?!<\/\2>).)*<\/\2>\s*<\/\1>/i }];
