8629c483a24fa44607e5303aefe55fdd547ad4df
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-06-30 12:14:22 -0400

    Daily Update - Find financial transaction IDs

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -3,3 +3,4 @@ const patterns = []; patterns.push({ hexEscape: { description: "Extract hexadeci
 const regexPatterns = [{ key: 'incorrectNesting', description: 'Finds incorrect HTML nesting', regex: /<([a-z]+)>(?:(?!<\/\1>).)*<([a-z]+)>(?:(?!<\/\2>).)*<\/\2>\s*<\/\1>/i }];
 const screenplayRegex = { screenplayFormat: { description: "Matches screenplay formatting: Scene Heading, Character, Dialogue, Parenthetical, and Action", regex: /^(INT\.|EXT\.) [A-Z ]+ \d{1,2}:\d{2} (AM|PM)$/gm } };
 const aerospacePartNumberRegex = { aerospacePartNumber: { description: "Validates aerospace part numbers (format: 1234-ABCD-5678)", regex: /^[0-9]{4}-[A-Z]{4}-[0-9]{4}$/ } };
+const regexArray = [{ key: 'cryptoTicker', description: 'Extract cryptocurrency ticker symbols', regex: /(?<!\w)([A-Z]{2,6})(?!\w)/g }];
