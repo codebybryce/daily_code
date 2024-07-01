a4c2c5e2e348269de4bab82d260711faa4e83e19
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-07-01 12:14:52 -0400

    Daily Update - Match airline flight numbers

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -4,3 +4,4 @@ const regexPatterns = [{ key: 'incorrectNesting', description: 'Finds incorrect 
 const screenplayRegex = { screenplayFormat: { description: "Matches screenplay formatting: Scene Heading, Character, Dialogue, Parenthetical, and Action", regex: /^(INT\.|EXT\.) [A-Z ]+ \d{1,2}:\d{2} (AM|PM)$/gm } };
 const aerospacePartNumberRegex = { aerospacePartNumber: { description: "Validates aerospace part numbers (format: 1234-ABCD-5678)", regex: /^[0-9]{4}-[A-Z]{4}-[0-9]{4}$/ } };
 const regexArray = [{ key: 'cryptoTicker', description: 'Extract cryptocurrency ticker symbols', regex: /(?<!\w)([A-Z]{2,6})(?!\w)/g }];
+const transactions = []; transactions.push({ transactionID: { description: "Financial transaction ID", regex: /[A-Z]{3}-\d{4}-[A-Z]{2}/ } });
