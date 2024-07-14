f9db6f00c4a152bf13b3f8b06827f00c4ba8bec8
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-07-14 12:21:22 -0400

    Daily Update - Validate whitespace formatting

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -17,3 +17,4 @@ const regexPatterns = []; regexPatterns.push({ directoryPath: { description: "Ma
 const regexValidations = []; regexValidations.push({ titleValidation: { description: "Validates forum post titles: 5-100 characters, no special characters except hyphens and underscores", regex: /^[A-Za-z0-9][A-Za-z0-9-_ ]{4,95}[A-Za-z0-9]$/ } });
 const authorRegex = { authorExtract: { description: "Extract author names from papers", regex: /(?<=Author[s]?:?\s)(.*?)(?=\s*[\n\r]|$)/g } };
 const regexPatterns = [{ misplacedCommas: { description: "Find misplaced commas in lists", regex: /,(?=\s*,)/ } }];
+const regexArray = [{ key: 'leadingZeros', description: 'Match leading zeros in numbers', regex: /^0+/ }];
