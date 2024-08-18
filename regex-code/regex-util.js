689a64e0cf723b627aa155c02ce37daecbcca75b
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-08-18 12:38:52 -0400

    Daily Update - Match machine learning model names

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -53,3 +53,4 @@ const regexPatterns = [{ camelCase: { description: "Find camelCase variables", r
 const regexArray = []; regexArray.push({ unixPermissions: { description: "Matches Unix file permissions", regex: /^([drwx-]{10})$/ } });
 const cookieValidation = { cookieValue: { description: "Validates cookie values according to RFC 6265", regex: /^[!#$%&'*+.\-^_`|~0-9a-zA-Z]+(=[!#$%&'*+.\-^_`|~0-9a-zA-Z]*)?$/ } };
 const xmlAttrExtractor = { xmlAttr: { description: "Extract XML attribute values", regex: /<\w+\s+[^>]*?(\w+)="([^"]*?)"/g } };
+const patterns = []; patterns.push({ overlappingWords: { description: "Find overlapping word patterns", regex: /(?=(\b\w+\b))(?=\1)/g } });
