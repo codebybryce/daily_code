13fcd6e017230e295231cbe7c2df1cbfad225e29
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2024-11-03 13:17:22 -0500

    Daily Update - Find domain extensions in URLs

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -134,3 +134,4 @@ const morseCodeValidation = { morseCode: { description: "Validates Morse code in
 const tweetIDRegex = { tweetID: { description: "Extract tweet IDs", regex: /(?<=status\/|\/)(\d{10,})/g } };
 const regexPatterns = []; regexPatterns.push({ key: "removeUnnecessaryPunctuation", description: "Find and remove unnecessary punctuation", regex: /[^\w\s]|(?<!\w)\s+|\s+(?!\w)/g });
 const commentsRegex = { cStyleComments: { description: "Matches C-style comments", regex: /\/\*[\s\S]*?\*\/|\/\/.*$/gm } };
+const regexPatterns = []; regexPatterns.push({ hashtag: { description: "Validates hashtag formatting (must start with # followed by alphanumeric characters, underscores, or dashes)", regex: /^#[A-Za-z0-9_-]+$/ } });
