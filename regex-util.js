04fed646901fad8d72d2da0d762c79b1de5bc2da
Bryce Robinson <berobinson83@gmail.com>
2025-02-10 01:18:05 +0000

    Daily Update - Extract invoice numbers from text

--- regex-util.js
+++ regex-util.js
@@ -1,1 +1,2 @@
 const emailRegex = { emailFinder: { description: "Find hidden email addresses in text", regex: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g } };
+const invoiceRegex = { invoiceNumber: { description: "Extract invoice numbers", regex: /\bINV-\d{4}-\d{6}\b/g } };
