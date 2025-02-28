3526489409485bdce1f61510fa16615e0003b77b
Bryce Robinson <berobinson83@gmail.com>
2025-02-28 09:00:06 +0000

    Daily Update - Validate Amazon product IDs

--- regex-code/regex-util.js
+++ regex-code/regex-util.js
@@ -284,3 +284,4 @@ const qrCodeValidation = { qrCode: { description: "Validates QR code format", re
 const articlesRegex = { articles: { description: "Find references to articles", regex: /(?:article|ref)\s*(?:#|:)?\s*(\d+)/gi } };
 const regexPatterns = [{ key: 'cryptoWallet', description: 'Extract cryptocurrency wallet addresses', regex: /(?:0x[a-fA-F0-9]{40}|[13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[a-z0-9]{39,59})/ }];
 const regexPatterns = []; regexPatterns.push({ currency: { description: "Matches numbers with currency symbols", regex: /\$\d+(\.\d{2})?|\€\d+(\.\d{2})?|\£\d+(\.\d{2})?/ } });
+const productIdValidation = { amazonProductId: { description: "Validates Amazon product IDs (ASINs) which are 10 characters long and can contain letters and numbers.", regex: /^[A-Z0-9]{10}$/ } };
